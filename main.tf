843080c1e3cacdb19789cd4d834c31c70ab2be53
sbintus <45663663+sbintus@users.noreply.github.com>
2022-04-04 10:17:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
 provider "azurerm" {
   features{}
 }
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
 
 resource "azurerm_storage_account" "example" {
   name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = "testRG"
+  location                 = "East US"
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
