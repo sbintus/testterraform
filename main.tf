4eebcd14847f6ed3efe471b988a90ed9bf35c363
sbintus <45663663+sbintus@users.noreply.github.com>
2022-04-04 10:34:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,42 @@
-provider "azurerm" {
-  features{}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "sabinttest1234567"
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate${random_string.resource_code.result}"
   resource_group_name      = "testRG"
   location                 = "East US"
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
+  allow_blob_public_access = true
 
   tags = {
     environment = "staging"
   }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "blob"
 }
